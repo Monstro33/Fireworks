c35485bea16e04343dd1e066885e59927e564aed
Michael Manville <monstro33@yahoo.com>
2019-03-19 15:00:22 -0400

    First Commit

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -207,6 +207,21 @@ var _fireworksCanvas = _interopRequireDefault(require("fireworks-canvas"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var options = {
+  maxRockets: 3,
+  // max # of rockets to spawn
+  rocketSpawnInterval: 150,
+  // millisends to check if new rockets should spawn
+  numParticles: 100,
+  // number of particles to spawn when rocket explodes (+0-10)
+  explosionMinHeight: 0.2,
+  // percentage. min height at which rockets can explode
+  explosionMaxHeight: 0.9,
+  // percentage. max height before a particle is exploded
+  explosionChance: 0.08 // chance in each tick the rocket will explode
+
+};
+
 var _default = function _default() {
   var container = document.getElementById('container');
   var fireworks = new _fireworksCanvas.default(container);
